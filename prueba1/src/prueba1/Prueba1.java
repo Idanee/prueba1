2d7b87acccb6de47bc1ffa12afa9f0fbf40972ed
Alumno <Alumno@10.25.0.13>
2017-02-13 16:04:28 -0600

    Se agregó boton cerrar

--- prueba1/src/prueba1/Prueba1.java
+++ prueba1/src/prueba1/Prueba1.java
@@ -5,6 +5,11 @@
  */
 package prueba1;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -21,7 +26,22 @@ public class Prueba1 {
        JFrame ventana1= new JFrame("Ventana de prueba1");
        ventana1.setSize(320,200);
        ventana1.setVisible(true);
+       ventana1.setLayout(new FlowLayout());
+       JButton btn1=new JButton("Cerrar");
+       btn1.addActionListener(new ActionListener(){
+       
+           @Override
+           public void actionPerformed(ActionEvent e){
+           System.exit(0);
+           
+           }
+           
+           }
+       );
+           
+      
         // TODO code application logic here
     }
+    
     
 }
